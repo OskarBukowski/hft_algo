aadaa2522fde358f2def510d6f4730d47141b23d
ccxAlpha <oskar.bukowski1@gmail.com>
2022-04-10 12:00:36 +0200

    changed path to dynamic

--- db_ex_connections/start_ex.sh
+++ db_ex_connections/start_ex.sh
@@ -3,5 +3,5 @@
 PATH_TO_EX=$(pwd)
 
 for arg in "$@"; do
-  python3 "$PATH_TO_EX"/start_stop_all.py --start_ex "$arg"
+  python3 "$PATH_TO_EX"/db_ex_connections/start_stop_all.py --start_ex "$arg"
 done
--- db_ex_connections/stop_ex.sh
+++ db_ex_connections/stop_ex.sh
@@ -4,5 +4,5 @@
 PATH_TO_EX=$(pwd)
 
 for arg in "$@"; do
-  python3 "$PATH_TO_EX"/start_stop_all.py --stop_ex "$arg"
+  python3 "$PATH_TO_EX"/db_ex_connections/start_stop_all.py --stop_ex "$arg"
 done
